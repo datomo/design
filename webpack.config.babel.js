b0f981890bf32df4517596431d57989343856d2d
datomo <dave.lengw@gmail.com>
2017-05-02 10:44:53 +0200

    changed styles to css

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -24,7 +24,7 @@ let config = {
       path.resolve(__dirname, 'templates/post.pug')
     ],    
     'css/application': [
-      path.resolve(__dirname, 'assets/styles/application.scss')
+      path.resolve(__dirname, 'assets/css/application.scss')
     ]
   },
   output: {
