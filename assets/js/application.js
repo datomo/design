bdec8deb460ebd4b1c3ec9a373cc194dd5c912ec
datomo <dave.lengw@gmail.com>
2017-05-08 17:19:11 +0200

    implemented my own translator tool

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,1 +1,43 @@
-console.log("test")
+import $ from 'jquery';
+
+$(document).ready(function(){
+  console.log("test");
+
+
+  ////////////////////////////////////////////////////////////
+  //Translations Tool
+  ////////////////////////////////////////////////////////////
+  //change the text on click to german, if it's not active
+  $('#en-button').on("click", function(){
+    if ($('#en-button').hasClass("is-active")){
+
+    }else {
+      $.getJSON("assets/js/translations/english.json", function (data) {
+        $.each(data, function(key, val) {
+          $("." + key).text(val)
+        })
+      })
+      $("#de-button").removeClass("is-active")
+      $("#en-button").addClass("is-active");
+    }
+  })
+
+  //change the text back to english, if it's not active
+  $('#de-button').on("click", function(){
+    if ($('#de-button').hasClass("is-active")){
+
+    }else {
+      $.getJSON("assets/js/translations/german.json", function (data) {
+        $.each(data, function(key, val) {
+          $("." + key).text(val)
+        })
+      })
+      $("#en-button").removeClass("is-active")
+      $("#de-button").addClass("is-active");
+    }
+  })
+
+  ////////////////////////////////////////////////////////////
+  //end
+  ////////////////////////////////////////////////////////////
+});
